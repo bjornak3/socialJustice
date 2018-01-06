5c89937269710c458cef1e1f10ad9ef1f7747752
Miel Satrapa <if16b078@technikum-wien.at>
2018-01-06 17:56:55 +0100

    a

--- htmlFiles/DB/content.php
+++ htmlFiles/DB/content.php
@@ -6,22 +6,29 @@
         }
     switch($_GET['section']) {
         case 'home':
-            include ("search.php");
+            include("search.php");
             break;
         case 'profiles':
-            include ("profiles.php");
+            include("profiles.php");
             break;
         case 'posts':
-            include ("posts.php");
+            include("posts.php");
             break;
         case 'addnewprofile':
-            if (isset($_SESSION["Loggedin"])){
-                include ("addnewprofile.php");
+            if (isset($_SESSION["Loggedin"])) {
+                include("addnewprofile.php");
                 break;
+            } else {
+                include("notloggedin.php");
             }
-            else{
-                include ("notloggedin.php");
+            break;
+        case 'addnewpost':
+            if (isset($_SESSION["Loggedin"])) {
+                include("addnewpost.php");
+                break;
+            } else {
+                include("notloggedin.php");
             }
-            break;    }
+    }
 ?>
 </div>
