1f43b75fb408c05ee4f1e5af5945ca73ff44b4f2
Bjorna Kalaja <bjorna.kalaja@bemindo.com>
2018-01-07 12:46:23 +0100

    login done

--- htmlFiles/DB/loginconf.php
+++ htmlFiles/DB/loginconf.php
@@ -5,6 +5,32 @@ if (isset($_POST["Logout"]))
     header("Location: index.php");
     return;
 }
+	$servername = "(LocalDb)\MSSQLLocalDB";
+	$connectionInfo = array("Database"=>"SocialJustice");
+	$connection = sqlsrv_connect($servername, $connectionInfo);	
+	
+	$username = $_POST['Username'];
+	$password = $_POST['Password'];
+	$loginError = false; 
 if (isset($_POST["Login"]))  {
-    $_SESSION["Loggedin"] = "miel";
+	
+	if(isset($username) && isset($password)){
+		$selectUsr = "select USERNAME, password from USERS where USERNAME = '$username'";
+		
+		$stmt = sqlsrv_query($connection, $selectUsr); 
+		
+		if(!$stmt){
+			die();
+		}
+		while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ){
+			
+			$pwd = $row['password'];
+			if($pwd == md5($password)){
+				$_SESSION['Loggedin'] = $username;
+			}else{
+				$loginError = true; 
+			}
+		}
+		
+	}
 }
