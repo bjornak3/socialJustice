8f9c7c137a5842547e4e4db45917534b15834fae
Bjorna Kalaja <bjorna.kalaja@bemindo.com>
2018-01-07 13:43:34 +0100

    search profiles

--- htmlFiles/DB/profiles.php
+++ htmlFiles/DB/profiles.php
@@ -6,21 +6,22 @@ $connection = sqlsrv_connect($servername, $connectionInfo);
 if (isset($_POST["searchTwitterHandles"])){
     $twitterHandle = ($_POST["twitterHandle"]);
 
-    $getTwitterHandles = "select * from getProfileInformation where twitter_handle = '$twitterHandle'";
-    echo "<div style='padding-top: 10em;'> <p>Profiles here </p></div>";
-	$stmt = sqlsrv_query($connection, $getTwitterHandles);
+    $getTwitterHandles = "select profiles.VORNAME, profiles.NACHNAME, profiles.GESCHLECHT, PROFILES.job, HATE_LVL.NAME, profiles.twitter_handle from profiles, HATE_LVL
+where TWITTER_HANDLE = '$twitterHandle' and HATE_LVL.ID = (select PROFILES.FK_HATE_ID from PROFILES where TWITTER_HANDLE = '$twitterHandle');";
+	
+  $stmt = sqlsrv_query($connection, $getTwitterHandles);
 	if ($stmt === false){
+ 	
 		die(print_r(sqlsrv_errors(), true));
 	}
-	
-	$row_count = sqlsrv_num_rows($stmt);
-	if($row_count <= 0){
-		echo "<div style='padding-top: 10.05em;'> <p> No results found </p> </div>";
-	}else{
+
 		while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ){
-			echo "<br>". $row['vorname'] . " , ".$row['nachname']; 
+		echo "<div style='padding-top: 10em;'>
+			". $row['VORNAME'] . " " . $row['NACHNAME'] . " (".$row['twitter_handle'] .") <br>
+			Image: <img src = 'https://twitter.com/$twitterHandle/profile_image?size=original'> <br> Job: " 
+			. $row['job'] ."<br>Geschlecht: " . $row['GESCHLECHT']. " <br> Hate_Level: " . $row['NAME'];
 		}
-	}
+	
     return;
 } 
 ?>
